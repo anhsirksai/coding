83fab6b541fdc7581ef199331f780c59b0fa1a9e
sai krishna sripada <krishna1256@gmail.com>
2015-10-18 06:58:41 +0530

    how to work on windows

--- js/node/test.js
+++ js/node/test.js
@@ -1,2 +1,5 @@
-var sys = require("sys");  
+// How to run in windows:
+// open command prompt
+// node \path\to\file
+var sys = require("sys");
 sys.puts("Hello World");
---
+++ python/windows-pycharm-testing.py
@@ -0,0 +1,8 @@
+__author__ = 'saikrishna'
+
+# running on windows:
+# write the code here.
+# open command prompt
+# python <filename>
+
+print "saikrishna"
